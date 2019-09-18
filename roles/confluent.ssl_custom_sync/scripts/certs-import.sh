e278ad618ee2041a2a6689c15bbd1b1e25168869
efbar <17676301+efbar@users.noreply.github.com>
2019-09-18 17:48:58 +0200

    import priv key

--- roles/confluent.ssl_custom_sync/scripts/certs-import.sh
+++ roles/confluent.ssl_custom_sync/scripts/certs-import.sh
@@ -13,12 +13,12 @@ echo "------------------------------- $SERVICE -------------------------------"
 # create truststore in service. Adding alias CA-root to the certificate, importing the public ca-cert of the CA.
 keytool -keystore "$DIR/$SERVICE.truststore.jks" -alias CARoot -import -file "$DIR/ca-cert" -storepass $2 -noprompt -v
 
+# import private key in service keystore
+keytool -importkeystore -deststorepass $2 -destkeystore "$DIR/$SERVICE.keystore.jks" -srckeystore "$DIR/$SERVICE.keystore.p12" -srcstorepass $2 -srcstoretype PKCS12 -noprompt -v
+
 # import new certificates in service keystore.
 # import public ca-cert in keystore.
 keytool -keystore "$DIR/$SERVICE.keystore.jks" -alias CARoot -import -file "$DIR/ca-cert" -storepass $2 -noprompt -v
 
 # import signed certificate of the service in keystore.
 keytool -keystore "$DIR/$SERVICE.keystore.jks" -alias localhost -import -file "$DIR/crt" -storepass $2 -noprompt -v
-
-# import private key in service keystore
-keytool -import -alias localhost -file "$SERVICE.priv_key.pem" -keystore "$DIR/$SERVICE.keystore.jks"
